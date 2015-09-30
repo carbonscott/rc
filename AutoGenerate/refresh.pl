9b6f129ae60c4636bfa4c3ba7e36950bd5b11f10
Scott <wangimagine@gmail.com>
2015-09-29 22:23:04 -0500

    refresh.pl

---
+++ AutoGenerate/refresh.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use diagnostics;
+use feature 'say';
+use File::Compare;
+
+my $file = "./chapters/set4.md";
+my $file2 = "./chapters/set4_bk.md";
+my $command = 'cat '. $file .'> main.md && pandoc -V fontsize=12pt -V documentclass=article -V mainfont="Times New Roman" -H header.sty main.md -o output.pdf --latex-engine=xelatex';
+
+while (1) {
+	if (compare($file, $file2) == 0) {
+	}
+	else {
+		system($command);
+		system("cp ".$file." ".$file2);
+		say "-------COMPILED--------";
+	}
+	sleep (1);
+}
