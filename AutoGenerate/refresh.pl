a3c10bb5251de2ed062eb187371bd1d5027e4e42
Scott <wangimagine@gmail.com>
2015-10-18 16:26:39 -0500

    git

--- AutoGenerate/refresh.pl
+++ AutoGenerate/refresh.pl
@@ -6,8 +6,11 @@ use diagnostics;
 use feature 'say';
 use File::Compare;
 
-my $file = "./chapters/set4.md";
-my $file2 = "./chapters/set4_bk.md";
+# my $file = "./chapters/set6.md";
+# my $file2 = "./chapters/set6_bk.md";
+my $file = shift @ARGV;
+my $file2 = $file."_bk";
+system('cp '.$file.' '.$file2);
 my $command = 'cat '. $file .'> main.md && pandoc -V fontsize=12pt -V documentclass=article -V mainfont="Times New Roman" -H header.sty main.md -o output.pdf --latex-engine=xelatex';
 
 while (1) {
--- settings/highlightings.vim
+++ settings/highlightings.vim
@@ -9,7 +9,8 @@ au insertenter * hi CursorLineNR cterm=bold ctermbg=2
 au InsertLeave * hi CursorLineNR cterm=bold ctermbg=12
 
 "colorscheme
-colorscheme visualstudio
+"colorscheme visualstudio
+colorscheme github
 " setting for matrix colorscheme
 " colorscheme matrix
 " hi CursorLine guibg=#113311 guifg=white
