b7721a998a0c5a9fa67c8f234b92aff705dcc7df
carbonscott <wangimagine@gmail.com>
2017-05-10 11:31:48 -0500

    Add the feature of "today I found", in which I put things that is interesting for me.

--- perlscripts/log_choices.pl
+++ perlscripts/log_choices.pl
@@ -10,13 +10,15 @@ my $selection = prompt 'Choose a type of log: ',
 																-menu => {
 																				schedule => "slog",
 																				note     => "nlog",
-																				quote    => "quote"
+																				quote    => "quote",
+																				qq(today I found) => "tif",
 																}, ">";
 
 my $commands = {
 				slog => "vi ~/Dropbox/00-Workflow/00-workflow.txt",
 				nlog => "vi ~/Dropbox/00-Workflow/10-notes.txt",
-				quote=> "vi ~/.vim/settings/quotes"
+				quote=> "vi ~/.vim/settings/quotes",
+				tif  => "vi ~/Dropbox/00-Workflow/20-today_I_found.txt",
 };
 
 system($commands->{$selection}) == 0 or die("The command cannot be executed at ".$?);
