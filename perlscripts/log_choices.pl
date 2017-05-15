b2d56da03f0cb56ae3e1f9207da5d51b0a285516
carbonscott <wangimagine@gmail.com>
2017-05-15 15:12:08 -0500

    Add a new feature. When the one of log tasks exits, it keeps asking the next log unless exit is selected.

--- perlscripts/log_choices.pl
+++ perlscripts/log_choices.pl
@@ -6,19 +6,29 @@ use warnings;
 use feature qw(say);
 use IO::Prompter;
 
-my $selection = prompt 'Choose a type of log: ', 
-																-menu => {
-																				schedule => "slog",
-																				note     => "nlog",
-																				quote    => "quote",
-																				qq(today I found) => "tif",
-																}, ">";
-
 my $commands = {
 				slog => "vi ~/Dropbox/00-Workflow/00-workflow.txt",
 				nlog => "vi ~/Dropbox/00-Workflow/10-notes.txt",
 				quote=> "vi ~/.vim/settings/quotes",
 				tif  => "vi ~/Dropbox/00-Workflow/20-today_I_found.txt",
+				exit => "exit"
 };
 
-system($commands->{$selection}) == 0 or die("The command cannot be executed at ".$?);
+ASKING:
+while (1) {
+				my $selection = prompt 'Choose a type of log: ', 
+																-menu => {
+																				schedule => "slog",
+																				note     => "nlog",
+																				quote    => "quote",
+																				qq(today I found) => "tif",
+																				exit     => "exit"
+																}, ">";
+
+				if ($selection eq "exit") {
+								last ASKING;
+				} else {
+								system($commands->{$selection}) == 0 or die("The command cannot be executed at ".$?);
+				}
+}
+
