c8fa01a11b531604f882a22a390c7400e8d945d4
carbonscott <wangimagine@gmail.com>
2017-08-04 00:38:42 -0500

    add refresh for latex

---
+++ AutoGenerate/refresh_latex.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use diagnostics;
+use feature 'say';
+use File::Compare;
+
+my $file = shift @ARGV;
+my $file2 = $file."_bk";
+system('cp '.$file.' '.$file2);
+# my $command = 'cat '. $file .'> main.md && pandoc -V fontsize=12pt -V documentclass=article -V mainfont="Times New Roman" -H header.sty main.md -o output.pdf --latex-engine=xelatex';
+
+my $command = 'pdflatex '.$file;
+
+while (1) {
+	if (compare($file, $file2) == 0) {
+	}
+	else {
+		system($command);
+		system("cp ".$file." ".$file2);
+		say "-------COMPILED--------";
+	}
+	sleep (1);
+}
