ab8f38cc183fac4b98d3108a205a1e154735af75
carbonscott <wangimagine@gmail.com>
2017-12-09 23:40:00 -0600

    autodetect bib for pandoc

--- AutoGenerate/refresh_html.pl
+++ AutoGenerate/refresh_html.pl
@@ -9,14 +9,18 @@ use File::Compare;
 my $file = shift @ARGV;
 my $file2 = $file."_bk";
 system('cp '.$file.' '.$file2);
-my $command = 'cat '. $file .'> main.md && pandoc --mathjax -s main.md -o output.html';
+my $bib = "";
 
 while (1) {
 	if (compare($file, $file2) == 0) {
 	}
 	else {
+  $bib = "";
+  $bib = "--bibliography References/*.bib" if glob("References/*.bib");
+  my $command = 'cat '. $file .'> main.md && pandoc '.$bib.' --mathjax -s main.md -o output.html';
 		system($command);
 		system("cp ".$file." ".$file2);
+  say $command;
 		say "-------COMPILED--------";
 	}
 	sleep (1);
