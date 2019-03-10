57b2af64b76b2592ee687578125be749edaf99f0
Cong Wang <wangimagine@gmail.com>
2019-03-10 12:00:25 -0500

    update bash executable

--- rc/bash_executable.sh
+++ rc/bash_executable.sh
@@ -5,13 +5,13 @@
 # Set a lib path for Python to look for its modules ...
 export PYTHONPATH="$HOME/Dropbox/Projects:$PYTHONPATH"
 export PYTHONPATH="$HOME/Dropbox/Codes:$PYTHONPATH"
-#    export PYTHONPATH="$HOME/Dropbox/Codes/inSituX:$PYTHONPATH"
+export PYTHONPATH="$HOME/Dropbox/Codes/inSituX.Ctrl/:$PYTHONPATH"
 
 # Database library ...
 export PATH="$HOME/Dropbox/Projects/Define:$PATH"
 
 # PDF split ...
-export PATH="$HOME/Dropbox/Projects/PDF.Utility:$PATH"
+export PATH="$HOME/Dropbox/utilities/pdf_utility:$PATH"
 
 # inSituX.Ctrl -- serial crystallography software ...
 export PATH="$HOME/Dropbox/Codes/inSituX.Ctrl:$PATH"
