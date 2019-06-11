d63ef1c5a3bba486e7957cf0d77e151d85968f43
Cong Wang <wangimagine@gmail.com>
2019-06-11 09:37:27 -0500

    tmux command updates

--- rc/bash_executable.sh
+++ rc/bash_executable.sh
@@ -3,24 +3,18 @@
 # ============================================================================
 
 # Set a lib path for Python to look for its modules ...
-## export PYTHONPATH="$HOME/Dropbox/Projects:$PYTHONPATH"
-## export PYTHONPATH="$HOME/Dropbox/Codes:$PYTHONPATH"
-## export PYTHONPATH="$HOME/Dropbox/Codes/inSituX.Ctrl/:$PYTHONPATH"
+export PYTHONPATH="$HOME/Dropbox/codes/inSituX/:$PYTHONPATH"
 export PYTHONPATH="$HOME/Dropbox/codes:$PYTHONPATH"
-
-# Database library ...
-export PATH="$HOME/Dropbox/Projects/Define:$PATH"
 
 # PDF split ...
 export PATH="$HOME/Dropbox/utilities/pdf_utility:$PATH"
 
 # inSituX.Ctrl -- serial crystallography software ...
-export PATH="$HOME/Dropbox/Codes/inSituX.Ctrl:$PATH"
+export PATH="$HOME/Dropbox/codes/inSituX.Ctrl:$PATH"
 
 # inSituX -- serial crystallography software ...
-export PATH="$HOME/Dropbox/Codes/inSituX:$PATH"
-#    export PATH="$HOME/Dropbox/Projects/SerialX/Research/inSituX:$PATH"
+export PATH="$HOME/Dropbox/codes/inSituX:$PATH"
 
 # FTP server -- serial crystallography software ...
-export PATH="$HOME/Dropbox/Projects/FTP.server:$PATH"
-export PATH="$HOME/Dropbox/Projects/FTP.server/Client:$PATH"
+export PATH="$HOME/Dropbox/projects/FTP.server:$PATH"
+export PATH="$HOME/Dropbox/projects/FTP.server/Client:$PATH"
