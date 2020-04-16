240bdadc38e726b3121953eb14c262ff8e58ef7a
Cong Wang <wangimagine@gmail.com>
2020-04-16 12:54:05 -0500

    add new

---
+++ rc/bash_executable.linux.sh
@@ -0,0 +1,19 @@
+# ============================================================================
+# Python executable path ...
+# ============================================================================
+
+# Python path...
+export PYTHONPATH=""
+export PYTHONPATH="$HOME/Dropbox/codes:$PYTHONPATH"
+
+# PDF split ...
+export PATH="$HOME/Dropbox/utilities/pdf_utility:$PATH"
+
+# inSituX -- serial crystallography software ...
+export PATH="$HOME/Dropbox/codes/inSituX:$PATH"
+
+# pme for pandoc, markdown and entr...
+export PATH="$HOME/Dropbox/codes/pme:$PATH"
+
+# texpro for LaTeX...
+export PATH="$HOME/Dropbox/codes/texpro:$PATH"
