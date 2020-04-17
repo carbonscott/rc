d1746bdd79d92adcbade14adacc4ed3510596ef1
Cong Wang <wangimagine@gmail.com>
2020-04-17 12:34:08 -0500

    add code to set dark mode for an X11 window application

--- rc/bash_env.sh
+++ rc/bash_env.sh
@@ -9,3 +9,9 @@ alias init_phenix='source /Applications/phenix-1.15-3459/phenix_env.sh'
 
 # CCP4 env
 alias init_ccp4='source /Applications/ccp4-7.0/bin/ccp4.setup-sh'
+
+
+## # Set dark theme for xterm-256color at X11 Window environment...
+## if [ "$TERM" == "xterm" ] ; then
+##     xprop -f _GTK_THEME_VARIANT 8u -set _GTK_THEME_VARIANT "dark" -id `xprop -root | awk '/^_NET_ACTIVE_WINDOW/ {print $5}'`
+## fi
