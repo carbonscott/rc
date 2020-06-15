2ae16fa538c1bc35ec6987ddac8d98a2b074a7df
Cong Wang <wangimagine@gmail.com>
2020-06-15 13:57:53 -0500

    update ipe configuration

---
+++ ipe/.ipe/ipelets/setDPI.lua
@@ -0,0 +1,12 @@
+label = "Set DPI"
+
+about = [[
+Set DPI
+]]
+
+function run(model)
+  local str = model:getString("Enter DPI")
+  if not str or str:match("^%s*$") then return end
+  local resolution = tonumber(str)
+  model.ui:setZoom(resolution / 72.0)
+end 
