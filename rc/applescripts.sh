7b7b10652cd21e789b8e12f3cea2fcdcf89e4625
Cong Wang <wangimagine@gmail.com>
2021-08-02 15:44:30 -0500

    Support going/jumping to a user specified terminal

--- rc/applescripts.sh
+++ rc/applescripts.sh
@@ -6,22 +6,21 @@ function tc {
     osascript -e "tell application \"Terminal\" to every window whose name contains \"$1\""
 }
 
+# Go to a specific terminal window
 function tg {
-    osascript -e "tell application \"Terminal\" to set frontmost of window id $1 to true"
+    osascript -e "tell application \"Terminal\"
+        set wintext to (every window whose name contains \"$1\")
+        set winitem to item 1 of wintext
+        set frontmost of winitem to true
+    end tell"
 }
 
-function ttd {
-    osascript -e 'tell application "Terminal" to set current settings of front window to settings set "Directory"'
-}
-
-function ttc {
-    osascript -e 'tell application "Terminal" to set current settings of front window to settings set "Code White"'
-}
-
-function ttw {
-    osascript -e 'tell application "Terminal" to set current settings of front window to settings set "Novel"'
-}
-
-function ttp {
-    osascript -e 'tell application "Terminal" to set current settings of front window to settings set "Code SCP"'
+# Go to a specific terminal window and kill the current window
+function tj {
+    osascript -e "tell application \"Terminal\"
+        set wintext to (every window whose name contains \"$1\")
+        set winitem to item 1 of wintext
+        set frontmost of winitem to true
+    end tell"
+    exit
 }
