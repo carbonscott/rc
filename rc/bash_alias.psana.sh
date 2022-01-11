4ba69e845eeea675905ba16f9615734e93f21f45
Cong Wang <wangimagine@gmail.com>
2022-01-11 15:58:07 -0800

    Update bash alias for psana

---
+++ rc/bash_alias.psana.sh
@@ -0,0 +1,48 @@
+# Set neovim as vi ...
+## alias vi="nvim "
+alias vi="vim "
+
+# Typos ...
+alias sl="ls "
+
+# Shorter grep ...
+alias gp="grep --color -e "
+alias gpi="grep --color -i -e "
+alias gpiv="grep --color -iv -e "
+
+# Apply function on each item filtered by xargs ...
+alias sargs="xargs -I {} -n 1 "
+
+# Easier ls ...
+alias lst="ls -lrt "
+
+# Add u+x to a file...
+alias x1="chmod u+x "
+
+# Tmux related...
+alias tls='tmux list-sessions'
+alias tas='tmux attach-session -t '
+alias tns='tmux new-session -s '
+
+# more or less
+alias m='more'
+alias l='less'
+
+# Go to example directory...
+alias e='cd $HOME/Dropbox/codes/examples'
+
+# Go to article directory...
+alias q='cd $HOME/Dropbox/notes/quicknotes'
+
+# Go to English directory...
+alias n='cd $HOME/Dropbox/resources/English'
+
+# Go to code directory...
+alias c='cd $HOME/Dropbox/codes'
+
+# Go to Dropbox directory...
+alias d='cd $HOME/Dropbox'
+
+
+# About using virtualenv for Python environment...
+alias active="source ./venv/bin/activate"
