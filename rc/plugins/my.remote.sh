b7e98d018b91e644fd79175bbb5f9a31a677b9fc
Cong Wang <wangimagine@gmail.com>
2023-07-08 15:42:37 -0700

    Support proxy jump with ssh.

--- rc/plugins/my.remote.sh
+++ rc/plugins/my.remote.sh
@@ -1,3 +1,24 @@
+function pslogin.login {
+    ssh -t -X cwang31@pslogin.slac.stanford.edu BG_COLOR=$BG_COLOR exec bash
+}
+
+###############################################################################
+# The following ssh session requires proxy jump.  
+# Example config needs to be set up in $HOME/.ssh/config
+#
+# Host pslogin.slac.stanford.edu
+#   HostName pslogin.slac.stanford.edu
+#   User cwang31
+#
+# Host psana
+#   HostName psana
+#   User cwang31
+#   ProxyJump pslogin.slac.stanford.edu
+#
+# Host perlmutter.nersc.gov
+#   HostName perlmutter.nersc.gov
+#   User cwang31
+###############################################################################
 function psana.login {
-    ssh -t -X cwang31@psnxana.slac.stanford.edu BG_COLOR=$BG_COLOR exec bash
+    ssh -t -X cwang31@psana BG_COLOR=$BG_COLOR exec bash
 }
