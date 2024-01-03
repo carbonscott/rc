19ebc452516393f548d823ce42f4229333d41135
carbonscott <wangimagine@gmail.com>
2024-01-02 16:00:06 -0800

    Modify the repo.

--- rc/plugins/my.projects.sh
+++ rc/plugins/my.projects.sh
@@ -1,5 +1,5 @@
 # ENV for note-projects
-export DATA_PROJECTS=$DATA_ROOT/notes
+export DATA_PROJECTS=$DATA_ROOT/projects
 function my.projects.updatedb {
     export MY_MD_projects=`find $DATA_PROJECTS -iname '*.md'`
 }
